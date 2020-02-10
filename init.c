f3d8105354a2fc7b3ec23d9131ffb06cf555a64f
JoabTeruel <joab.gteruel@gmail.com>
2020-02-10 17:31:07 -0600

    programa init.c ya terminado

--- init.c
+++ init.c
@@ -5,26 +5,25 @@
 
 int main()
 {
-	int i;
 	int status;
-	pid_t p;
+	pid_t pfork;
 	char mypid[20];
+
 	sprintf(mypid, "%d", getpid());
-	//printf("Soy el proceso init, el padre, mi pid es: %d o %s\n", getpid(), mypid);
-	for(i=0;i<6;i++)
+
+	for(int i=0;i < 6;i++)
 	{
-		p=fork();
-		if(p==0) //es un proceso hijo
+		pfork = fork();
+		if(0 == pfork) //Proceso Hijo
 			execlp("xterm","xterm", "-e", "./getty", mypid, NULL);
 	}
-	//printf("Estado: %d", status);
 	while(1)
 	{
 		wait(&status);
 		if(status==3840)
 			execlp("pkill", "pkill", "-TERM", "-P", mypid, NULL);
-		p=fork();
-		if(p==0) //es un proceso hijo
+		pfork = fork();
+		if(0 == pfork) //si es un proceso hijo
 			execlp("xterm","xterm", "-e", "./getty", mypid, NULL);
 	}
 	
